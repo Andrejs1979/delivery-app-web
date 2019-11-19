5ea61d74f26a3c3884ef3de3de33d815635adccf
Andrey Morozov <am@payment.ninja>
2019-11-18 20:40:11 -0500

    it's working!!!

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -1,49 +1,4 @@
-const { ApolloServer, gql } = require('apollo-server-lambda');
-
-// const typeDefs = gql`
-// 	type Query {
-// 		hello: String
-// 		allAuthors: [Author!]
-// 		author(id: Int!): Author
-// 		authorByName(name: String!): Author
-// 	}
-
-// 	type Author {
-// 		id: ID!
-// 		name: String!
-// 		married: Boolean!
-// 	}
-// `;
-
-// const authors = [
-// 	{ id: 1, name: 'Terry Pratchett', married: false },
-// 	{ id: 2, name: 'Stephen King', married: true },
-// 	{ id: 3, name: 'JK Rowling', married: false }
-// ];
-
-// const resolvers = {
-// 	Query: {
-// 		hello: (root, args, context) => {
-// 			return 'Hello, world!';
-// 		},
-// 		allAuthors: (root, args, context) => {
-// 			return authors;
-// 		},
-// 		author: (root, args, context) => {
-// 			return;
-// 		},
-// 		authorByName: (root, args, context) => {
-// 			console.log('hihhihi', args.name);
-// 			return authors.find((x) => x.name === args.name) || 'NOTFOUND';
-// 		}
-// 	}
-// };
-
-// const server = new ApolloServer({
-// 	typeDefs,
-// 	resolvers
-// });
-
+const { ApolloServer } = require('apollo-server-lambda');
 const responseCachePlugin = require('apollo-server-plugin-response-cache');
 const mongoose = require('mongoose');
 // const Raven = require('raven');
@@ -75,9 +30,8 @@ if (process.env.NODE_ENV !== 'test') {
 const server = new ApolloServer({
 	schema,
 	plugins: [ responseCachePlugin() ],
-
-	context: async ({ req }) => {
-		const auth = req.headers.authorization || '';
+	context: async ({ event, context }) => {
+		const auth = event.headers.authorization || '';
 		const token = auth.slice(7);
 		const account = await Accounts.findByToken(token);
 
